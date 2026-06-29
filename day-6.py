d6a14e59398ddf177c9aa276368a1e6b231725c6
harshavardhini034-creator <harshavardhini034@gmail.com>
2026-06-30 00:29:06 +0530

    Add files via upload
    What I Learned Today
    Understanding data types is very important in Python.
    Type conversion helps avoid errors while performing operations.
    Python provides built-in functions to convert data easily.
    Learning these basics will help me write better and more efficient programs.
    
    Every new topic is helping me improve my programming skills step by step. I'm excited to continue learning and building projects with Python

---
+++ day-6.py
@@ -0,0 +1,103 @@
+'''
+type converstions
+------------------
+--> This is process of coverting one data type to another ....
+Int -->string;Float
+num =89
+num_2 =float (num)
+print(num_2)
+print(type num))
+
+Float: String;tuple;Dictionary
+-->
+eg
+--
+num = 8.9
+how = str(num)
+print(how)
+print(type(how))
+num_2 = int(num)
+print(num_2)
+
+string :Integer,list,tupel
+
+hai = "88"
+num = int(hai)
+print(num+10)
+hello = "67.89"
+num_2 =float(hello)
+print(num_2 + num)
+
+any_ ="python"
+con_ = list(any_)
+print(con_)
+
+any_ ="python"
+con_ = list(any_)
+print(con_)
+con_2 =tuple (any_)
+print(con_2)
+list : string;Tupel;Dictinary
+eg
+--
+var_ = ['p','y','t','h','o','n']
+text_ ="".join(var_)
+print(text_)
+eg
+--
+tuple : string;List
+var_ = ['p','y','t','h','o','n']
+some_ ="".join(var_)
+print(some)
+
+pyth_ = [('a',1),('b',8)]
+conver = dict(pyth_)
+print(conver)
+
+bullit-in funictions
+--------------------
+str()
+float()
+int()
+list()
+dict()
+
+fam = (1,2,3,4,)
+print(list(fam))'''
+
+fam = ('h','e','l','l','o')
+text_2 ="".join(fam)
+print(text_2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
